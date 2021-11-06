aa835535e4fa7ff01cbb393afc146dec2dd5bcc0
proegssilb <proegssilb@gmail.com>
2021-11-06 17:05:34 -0500

    Update to Rust 2021 (#1)
    * Setup
    
    * Take ownership
    
    * Run cargo fix --edition
    
    * cargo fmt: fix try-macros
    
    * cargo clippy --fix
    
    * Manually fix warning
    
    * edition upgrade

--- src/app/app.rs
+++ src/app/app.rs
@@ -1,32 +1,30 @@
-use output::OutputCollection;
-use sensor::SensorCollection;
+use crate::output::OutputCollection;
+use crate::sensor::SensorCollection;
 
 use std::thread;
 use std::time::Duration;
 
-use app::Config;
+use crate::app::Config;
 
 pub struct App {
-  sensors : SensorCollection,
-  outputs : OutputCollection,
+    sensors: SensorCollection,
+    outputs: OutputCollection,
 }
 
 impl App {
-  
-  pub fn from_config(config: Config) -> App {
-    App {
-      sensors : config.sensors,
-      outputs : config.outputs,
+    pub fn from_config(config: Config) -> App {
+        App {
+            sensors: config.sensors,
+            outputs: config.outputs,
+        }
     }
-  }
-  
-  pub fn run(&mut self) -> Result<(), String> {
-    try!(self.outputs.enable_all(true));
-    loop {
-      try!(self.sensors.update_all());
-      try!(self.outputs.update_all());
-      thread::sleep(Duration::from_secs(1));
+
+    pub fn run(&mut self) -> Result<(), String> {
+        self.outputs.enable_all(true)?;
+        loop {
+            self.sensors.update_all()?;
+            self.outputs.update_all()?;
+            thread::sleep(Duration::from_secs(1));
+        }
     }
-  }
-  
 }
--- src/input/evaluator.rs
+++ src/input/evaluator.rs
@@ -1,35 +1,41 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use input::Input;
-use parser::evaluator::TagEvaluator;
-use sensor::evaluator::NamedSensors;
+use crate::input::Input;
+use crate::parser::evaluator::TagEvaluator;
+use crate::sensor::evaluator::NamedSensors;
 
-use input::cutoff::EvalCutoff;
-use input::maximum::EvalMaximum;
-use input::panic::EvalPanic;
-use input::sensor::EvalSensorInput;
-use input::smooth::EvalSmooth;
-use input::step::EvalSteps;
+use crate::input::cutoff::EvalCutoff;
+use crate::input::maximum::EvalMaximum;
+use crate::input::panic::EvalPanic;
+use crate::input::sensor::EvalSensorInput;
+use crate::input::smooth::EvalSmooth;
+use crate::input::step::EvalSteps;
 
 // Reference to input evaluator
 pub type InputEvaluatorRef = Rc<RefCell<InputEvaluator>>;
-pub type InputEvaluator    = TagEvaluator<Box<Input>>;
+pub type InputEvaluator = TagEvaluator<Box<dyn Input>>;
 
 impl InputEvaluator {
-  pub fn create(named_sensors: Rc<RefCell<NamedSensors>>) -> InputEvaluatorRef {
-    let input_evaluator = Rc::new(RefCell::new(InputEvaluator::new()));
-    InputEvaluator::init(input_evaluator.clone(), named_sensors);
-    input_evaluator
-  }
-  
-  fn init(input_evaluator: Rc<RefCell<InputEvaluator>>, named_sensors: Rc<RefCell<NamedSensors>>) {
-    let mut input_borrow = input_evaluator.borrow_mut();
-    input_borrow.add("cutoff",       Rc::new(EvalCutoff     ::new(input_evaluator.clone())));
-    input_borrow.add("maximum",      Rc::new(EvalMaximum    ::new(input_evaluator.clone())));
-    input_borrow.add("panic",        Rc::new(EvalPanic      ::new(input_evaluator.clone())));
-    input_borrow.add("sensor-input", Rc::new(EvalSensorInput::new(named_sensors)));
-    input_borrow.add("smooth",       Rc::new(EvalSmooth     ::new(input_evaluator.clone())));
-    input_borrow.add("steps",        Rc::new(EvalSteps      ::new(input_evaluator.clone())));
-  }
+    pub fn create(named_sensors: Rc<RefCell<NamedSensors>>) -> InputEvaluatorRef {
+        let input_evaluator = Rc::new(RefCell::new(InputEvaluator::new()));
+        InputEvaluator::init(input_evaluator.clone(), named_sensors);
+        input_evaluator
+    }
+
+    fn init(
+        input_evaluator: Rc<RefCell<InputEvaluator>>,
+        named_sensors: Rc<RefCell<NamedSensors>>,
+    ) {
+        let mut input_borrow = input_evaluator.borrow_mut();
+        input_borrow.add("cutoff", Rc::new(EvalCutoff::new(input_evaluator.clone())));
+        input_borrow.add(
+            "maximum",
+            Rc::new(EvalMaximum::new(input_evaluator.clone())),
+        );
+        input_borrow.add("panic", Rc::new(EvalPanic::new(input_evaluator.clone())));
+        input_borrow.add("sensor-input", Rc::new(EvalSensorInput::new(named_sensors)));
+        input_borrow.add("smooth", Rc::new(EvalSmooth::new(input_evaluator.clone())));
+        input_borrow.add("steps", Rc::new(EvalSteps::new(input_evaluator.clone())));
+    }
 }
